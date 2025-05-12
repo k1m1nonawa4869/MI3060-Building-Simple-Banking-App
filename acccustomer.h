f2bdf215db2a83804e6560270001d0f790735dd9
Letatducmanh <letatducmanh2005@gmail.com>
2025-05-12 13:46:34 +0700

    Create acccustomer.h

---
+++ acccustomer.h
@@ -0,0 +1,16 @@
+#ifndef ACC_CUSTOMER_H
+#define ACC_CUSTOMER_H
+
+#include <string>
+#include <iostream>
+
+// Lớp tài khoản khách hàng 
+
+class Account {
+private: 
+    std::int accountId;
+    std::string CustomerName;
+    double balance;
+    std::string password;
+    
+}
